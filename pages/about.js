3116e91123f142c200c1950a5e84b9c2c4c27f13
Sourav <sourav0w@gmail.com>
2022-11-13 12:04:07 +0530

    Admin panel responsive

--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,26 @@ function About() {
       <Head>
         <title>Codex Souarv | About Me</title>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <meta content={process.env.HOST} property="og:url" />
+        <meta
+          content="codexsourav is Mainly about Latest Trending Updates , Trending Topics , Local News, Gaming News , and many more that&amp;#039;s make you update"
+          property="og:description"
+        />
+        <meta
+          content="codexsourav is Mainly about Latest Trending Updates , Trending Topics , Local News, Gaming News , and many more that&amp;#039;s make you update"
+          name="description"
+        />
+        <meta content="CodeX Sourav" property="og:site_name" />
+        <meta content="website" property="og:type" />
+        <meta content="CodeX Sourav" property="og:title" />
+        <meta content="CodeX Sourav" property="og:site_name" />
+        <meta content="summary" name="twitter:card" />
+        <meta content="CodeX Sourav" name="twitter:title" />
+        <meta content={process.env.HOST} name="twitter:domain" />
+        <meta
+          content="codexsourav is Mainly about Latest Trending Updates , Trending Topics , Local News, Gaming News , and many more that&amp;#039;s make you update"
+          name="twitter:description"
+        />
       </Head>
 
       <div className="container">
